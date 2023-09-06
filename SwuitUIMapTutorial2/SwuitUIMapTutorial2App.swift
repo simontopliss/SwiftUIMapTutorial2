06ebabbb949c3c9658d305a3f5ecd3e77d4035c8
simontopliss <simontopliss@me.com>
2023-09-06 18:58:37 +0100

    Initial Commit

---
+++ SwuitUIMapTutorial2/SwuitUIMapTutorial2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwuitUIMapTutorial2App.swift
+//  SwuitUIMapTutorial2
+//
+//  Created by Simon Topliss on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwuitUIMapTutorial2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
